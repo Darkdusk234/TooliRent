ee1412767b2df0195dc34de866318bb1d4e21b88
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-03 11:21:42 +0200

    Added model for tool entity in database

---
+++ TooliRent.Core/Models/Tool.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TooliRent.Core.Models
+{
+    public class Tool
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string ToolType { get; set; } = string.Empty;
+
+        public int CategoryId { get; set; }
+
+        [MaxLength(300)]
+        public string Description { get; set; } = string.Empty;
+
+        public bool Available { get; set; } = true;
+
+
+        //Navigational properties
+        public Category? Category { get; set; }
+    }
+}
