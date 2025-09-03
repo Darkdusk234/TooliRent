49a00e0b0ec365042c575e53c16cde88a494e029
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-03 11:22:48 +0200

    Added model for Category entity

---
+++ TooliRent.Core/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TooliRent.Core.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(255)]
+        public string Description { get; set; } = string.Empty;
+    }
+}
