a807c9431d3154e7dfabbcf875636af76f09a6ab
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-03 14:16:23 +0200

    Added a booking model

---
+++ TooliRent.Core/Models/Booking.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TooliRent.Core.Models
+{
+    public class Booking
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public int ToolId { get; set; }
+
+        public bool IsPickedUp { get; set; } = false;
+        public DateTime? ReturnDate { get; set; }
+
+        //Navigational Properties
+        public User? User { get; set; }
+        public Tool? Tool { get; set; }
+    }
+}
