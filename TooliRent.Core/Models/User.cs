9599070f9ba6da2ee71a8ade5cefcbfdc69d3870
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-03 14:01:56 +0200

    Added a user model that builds on model in Microsoft Identity

---
+++ TooliRent.Core/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace TooliRent.Core.Models
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime? BirthDate { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
