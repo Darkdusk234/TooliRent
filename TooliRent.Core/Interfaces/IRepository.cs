f50add1ce7879099fe906fc0ed034294c10067d6
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-07 11:20:16 +0200

    Created a generic repository interface with CRUD methods

---
+++ TooliRent.Core/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace TooliRent.Core.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+        Task<bool> ExistsAsync(int id);
+    }
+}
