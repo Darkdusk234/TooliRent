9678b1f17da6017d12a59399da790731bdbe3bbf
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-07 11:52:15 +0200

    Created BookingRepo and implemented GetActiveBookingAsync method

---
+++ TooliRent.Infrastructure/Repositories/BookingRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TooliRent.Core.Interfaces;
+using TooliRent.Core.Models;
+using TooliRent.Infrastructure.Data;
+
+namespace TooliRent.Infrastructure.Repositories
+{
+    public class BookingRepository : Repository<Booking>, IBookingRepository
+    {
+        public BookingRepository(ToolIRentDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Booking>> GetActiveBookingsAsync()
+        {
+            return await _dbSet.Where(b => b.ReturnDate == DateTime.MinValue).ToListAsync();
+        }
+
+        public Task<IEnumerable<Booking>> GetBookingsByPickupStatusAsync(bool isPickedUp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Booking>> GetBookingsByToolIdAsync(int toolId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Booking>> GetBookingsByUserIdAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Booking>> GetBookingsWithinDateRangeAsync(DateTime startDate, DateTime endDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
