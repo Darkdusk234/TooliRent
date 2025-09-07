68b3d3bb34c3e3dd9555561517f50f77ef243d18
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-07 11:47:16 +0200

    Implemented ToolRepository

---
+++ TooliRent.Infrastructure/Repositories/ToolRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TooliRent.Core.Interfaces;
+using TooliRent.Core.Models;
+using TooliRent.Infrastructure.Data;
+
+namespace TooliRent.Infrastructure.Repositories
+{
+    internal class ToolRepository : Repository<Tool>, IToolRepository
+    {
+        public ToolRepository(ToolIRentDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Tool>> GetToolsByAvailabilityAsync(bool available)
+        {
+            return await _dbSet.Where(t => t.Available == available).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Tool>> GetToolsByCategoryAsync(int categoryId)
+        {
+            return await _dbSet.Where(t => t.CategoryId == categoryId).ToListAsync();
+        }
+    }
+}
