a3fcf7fb27152bd25f3f3d57f79a4b77b32d191e
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-08 14:24:17 +0200

    Created dto for use when creating a booking

---
+++ TooliRent.Services/DTOs/CreateBookingDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TooliRent.Services.DTOs
+{
+    public class CreateBookingDto
+    {
+        public string UserId { get; set; } = string.Empty;
+        public int ToolId { get; set; }
+    }
+}
