ebe8c6640b88af48668dc7324106b2c614c0d990
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-08 14:46:06 +0200

    Created dto for use when updating a booking

---
+++ TooliRent.Services/DTOs/UpdateBookingDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TooliRent.Services.DTOs
+{
+    public class UpdateBookingDto
+    {
+        public string UserId { get; set; } = string.Empty;
+        public int ToolId { get; set; }
+        public bool IsPickedUp { get; set; }
+        public DateTime? ReturnDate { get; set; }
+    }
+}
