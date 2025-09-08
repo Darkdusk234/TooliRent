54b2e73858ccfdd23d0ad71c65ef54bf460b3128
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-08 13:24:51 +0200

    Created DTO for when showing tool data

---
+++ TooliRent.Services/DTOs/ToolDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TooliRent.Services.DTOs
+{
+    internal interface ToolDto
+    {
+        public int ToolId { get; set; }
+        public string ToolType { get; set; }
+        public string Description { get; set; }
+        string Category { get; set; }
+        public bool Available { get; set; }
+    }
+}
