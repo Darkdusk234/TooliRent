7f0d6d4e6424dd5c855fe72d8a357e0ab5f7c30a
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-08 13:40:42 +0200

    Updated dto

--- TooliRent.Services/DTOs/ToolDto.cs
+++ TooliRent.Services/DTOs/ToolDto.cs
@@ -3,9 +3,9 @@
     public class ToolDto
     {
         public int ToolId { get; set; }
-        public string ToolType { get; set; }
-        public string Description { get; set; }
-        string Category { get; set; }
+        public string ToolType { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string? Category { get; set; }
         public bool Available { get; set; }
     }
 }
