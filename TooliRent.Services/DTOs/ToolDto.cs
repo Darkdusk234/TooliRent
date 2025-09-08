75b1ad6dfc5b9ce47a2f6d87c707353096e5d9fc
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-08 13:57:23 +0200

    Changed variable name

--- TooliRent.Services/DTOs/ToolDto.cs
+++ TooliRent.Services/DTOs/ToolDto.cs
@@ -2,7 +2,7 @@
 {
     public class ToolDto
     {
-        public int ToolId { get; set; }
+        public int Id { get; set; }
         public string ToolType { get; set; } = string.Empty;
         public string? Description { get; set; }
         public string? Category { get; set; }
