75ba93473080ed604b4b989c57022067e993e3ad
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-08 13:39:29 +0200

    Created a dto for use when creating a new tool

---
+++ TooliRent.Services/DTOs/CreateToolDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TooliRent.Services.DTOs
+{
+    public class CreateToolDto
+    {
+        public string ToolType { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public int CategoryId { get; set; }
+        public bool Available { get; set; } = true;
+    }
+}
