887a6eddc8474c8ce7edc3c9681777d509e8bf46
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-08 14:07:01 +0200

    Created dto for when reading Bookings

---
+++ TooliRent.Services/DTOs/BookingDto.cs
@@ -0,0 +1,17 @@
+﻿namespace TooliRent.Services.DTOs
+{
+    public class BookingDto
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public string? UserFirstName { get; set; }
+        public string? UserLastName { get; set; }
+        public int ToolId { get; set; }
+        public string ToolType { get; set; } = string.Empty;
+        public string? ToolDescription { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsPickedUp { get; set; }
+        public DateTime? ReturnDate { get; set; }
+
+    }
+}
