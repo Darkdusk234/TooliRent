045b8d0969a5af7379ff6e49d7081f8d030e91eb
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-10 14:42:48 +0200

    Created dto fo use when reading categories

---
+++ TooliRent.Services/DTOs/CategoryDtos/CategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TooliRent.Services.DTOs.CategoryDtos
+{
+    internal class CategoryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
