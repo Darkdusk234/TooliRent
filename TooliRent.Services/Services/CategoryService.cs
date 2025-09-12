e791bb9a746394e3de81b83303a340a3b516bc78
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-12 10:46:59 +0200

    Created Categoryservice file using the Categoryservice interface

---
+++ TooliRent.Services/Services/CategoryService.cs
@@ -0,0 +1,38 @@
+﻿using TooliRent.Services.DTOs.CategoryDtos;
+using TooliRent.Services.Interfaces;
+
+namespace TooliRent.Services.Services
+{
+    internal class CategoryService : ICategoryService
+    {
+        public Task<IEnumerable<CategoryDto>> GetAllCategoriesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CategoryDto?> GetByCategoryIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CreateCategoryDto> CreateCategoryAsync(CreateCategoryDto category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UpdateCategoryDto> UpdateCategoryAsync(UpdateCategoryDto category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CategoryExistsAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
