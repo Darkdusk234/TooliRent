0f7e167742cef56ce02d280a18eeba4ccad10e5d
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-13 11:49:12 +0200

    added method to check if a tool is available to be booked.

--- TooliRent.Core/Interfaces/IToolRepository.cs
+++ TooliRent.Core/Interfaces/IToolRepository.cs
@@ -6,6 +6,7 @@ namespace TooliRent.Core.Interfaces
     {
         public Task<IEnumerable<Tool>> GetToolsByCategoryAsync(int categoryId);
         public Task<IEnumerable<Tool>> GetToolsByAvailabilityAsync(bool available);
+        public Task<bool> IsToolAvailableAsync(int toolId);
         // Additional methods to maybe be added: Function to search for all tools still in the workshop by availability and if they have been pickuped 
         //in booking
     }
