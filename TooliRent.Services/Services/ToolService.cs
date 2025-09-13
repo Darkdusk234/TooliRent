e627fa6adddc150882fcdf1062f1a4d7f2d72b7f
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-13 10:36:27 +0200

    Created ToolService and implemented IToolService interface

---
+++ TooliRent.Services/Services/ToolService.cs
@@ -0,0 +1,53 @@
+﻿using TooliRent.Services.DTOs.ToolDtos;
+using TooliRent.Services.Interfaces;
+
+namespace TooliRent.Services.Services
+{
+    internal class ToolService : IToolService
+    {
+        public Task<ToolDto> CreateToolAsync(CreateToolDto createToolDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteToolAsync(int toolId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ToolDto>> GetAllToolsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ToolDto?> GetToolByIdAsync(int toolId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ToolDto>> GetToolsByAvailabilityAsync(bool available)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ToolDto>> GetToolsByCategoryAsync(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsToolAvailableAsync(int toolId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ToolExistsAsync(int toolId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateToolAsync(int toolId, UpdateToolDto updateToolDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
