ae168d331ed5e86ba5f5420f49741a8f92747011
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-16 09:34:16 +0200

    Created the controller for bookings

---
+++ TooliRent/Controllers/BookingsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TooliRent.Services.Interfaces;
+
+namespace TooliRent.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingsController : ControllerBase
+    {
+        private readonly IBookingService _bookingService;
+
+        public BookingsController(IBookingService bookingService)
+        {
+            _bookingService = bookingService;
+        }
+
+
+    }
+}
