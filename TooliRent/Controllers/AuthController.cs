6c2f8bd99d4206a90f9375b54e78392c4db22269
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-16 13:12:34 +0200

    Created AuthController

---
+++ TooliRent/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TooliRent.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _users;    // or your IUserService
+        private readonly IConfiguration _config;
+
+        public AuthController(UserManager<IdentityUser> users, IConfiguration config)
+        {
+            _users = users;
+            _config = config;
+        }
+    }
+}
