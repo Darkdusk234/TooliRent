3b69aeb2117c51b12f6714a23cf9072cc555e30f
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-16 13:14:03 +0200

    Created method to create JWTToken

--- TooliRent/Controllers/AuthController.cs
+++ TooliRent/Controllers/AuthController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace TooliRent.Controllers
 {
@@ -15,6 +19,33 @@ namespace TooliRent.Controllers
         {
             _users = users;
             _config = config;
+        }
+
+        private async Task<string> GenerateJwtTokenAsync(IdentityUser user)
+        {
+            var jwt = _config.GetSection("Jwt");
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt["Key"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var roles = await _users.GetRolesAsync(user);           // <-- fetch roles
+            var roleClaims = roles.Select(r => new Claim(ClaimTypes.Role, r));
+
+            var claims = new List<Claim>
+             {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName!),
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+            claims.AddRange(roleClaims);
+
+            var token = new JwtSecurityToken(
+                issuer: jwt["Issuer"],
+                audience: jwt["Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(double.Parse(jwt["ExpiresInMinutes"]!)),
+                signingCredentials: creds
+            );
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
