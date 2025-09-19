57db94e6b81d5b03e31424fbfd54cd03a3e974ee
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-19 11:29:06 +0200

    Created controller for categories

---
+++ TooliRent/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TooliRent.Services.Interfaces;
+
+namespace TooliRent.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+
+    }
+}
