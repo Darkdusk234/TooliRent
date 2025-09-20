4d91ab3f5b62eb844438fe79f6d268bffeaa135b
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-20 11:20:03 +0200

    Created ToolsController file

---
+++ TooliRent/Controllers/ToolsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TooliRent.Services.Interfaces;
+
+namespace TooliRent.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToolsController : ControllerBase
+    {
+        private readonly IToolService _toolService;
+
+        public ToolsController(IToolService toolService)
+        {
+            _toolService = toolService;
+        }
+    }
+}
