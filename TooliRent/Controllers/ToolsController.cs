63dcaee40a3c78701651dc3c66b46cdf42276e31
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-20 11:24:01 +0200

    Created call method to get all tools

--- TooliRent/Controllers/ToolsController.cs
+++ TooliRent/Controllers/ToolsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TooliRent.Services.DTOs.ToolDtos;
 using TooliRent.Services.Interfaces;
 
 namespace TooliRent.Controllers
@@ -15,6 +16,14 @@ namespace TooliRent.Controllers
         public ToolsController(IToolService toolService)
         {
             _toolService = toolService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<ToolDto>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAllTools()
+        {
+            var tools = await _toolService.GetAllToolsAsync();
+            return Ok(tools);
         }
     }
 }
