dd5673cae55ce8a20a0dcc8a3fd5e3b89fe59c44
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-23 14:10:32 +0200

    Removed unnessecary dto not in use anymore

--- TooliRent.Services/DTOs/ToolDtos/ToolInfoDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TooliRent.Services.DTOs.ToolDtos
-{
-    public class ToolInfoDto
-    {
-        public string ToolType { get; set; } = string.Empty;
-        public string? Description { get; set; }
-    }
-}
