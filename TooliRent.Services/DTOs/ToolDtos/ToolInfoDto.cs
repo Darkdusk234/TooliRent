c62ea3f3e8c20f1b6d10c7a6c43aa2aab67b7e4e
darkdusk234 <johan.svensson.01@hotmail.com>
2025-09-23 10:22:49 +0200

    Created toolinfodto for use in bookingdto

---
+++ TooliRent.Services/DTOs/ToolDtos/ToolInfoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TooliRent.Services.DTOs.ToolDtos
+{
+    public class ToolInfoDto
+    {
+        public string ToolType { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
